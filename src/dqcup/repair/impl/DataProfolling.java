85b8d3e4c4dd64dcf58ed3dd2c0e75684c96a38d
pishilong <pishilong@gmail.com>
2015-10-19 18:58:04 +0800

    重构代码；靠truthTuples驱动

--- src/dqcup/repair/impl/DataProfolling.java
+++ src/dqcup/repair/impl/DataProfolling.java
@@ -51,80 +51,18 @@ public class DataProfolling {
 		 */
 		for (Tuple tuple: tuples){
 			Tuple truthTuple = truthTuples.get(tuple.getValue("CUID"));
-			if (!checkSingleSSN(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"SSN", 
-						truthTuple.getValue("SSN")));
+			for (String field : FIELDS) {
+				if (!tuple.getValue(field).equals(truthTuple.getValue(field))) {
+					result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
+							field, 
+							truthTuple.getValue(field)));
+				}
 			}
-			if (!checkSingleFNAME(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"FNAME", 
-						truthTuple.getValue("FNAME")));
-			}
-			if (!checkSingleMINIT(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"MINIT", 
-						truthTuple.getValue("MINIT")));
-			}
-			if (!checkSingleLNAME(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"LNAME", 
-						truthTuple.getValue("LNAME")));
-			}
-			if (!checkSingleSTNUM(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"STNUM", 
-						truthTuple.getValue("STNUM")));
-			}
-			if (!checkSingleSTADD(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"STADD", 
-						truthTuple.getValue("STADD")));
-			}
-			if (!checkSingleAPMT(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"APMT", 
-						truthTuple.getValue("APMT")));
-			}
-			if (!checkSingleCITY(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"CITY",
-						truthTuple.getValue("CITY")));
-			}
-			if (!checkSingleSTATE(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"STATE", 
-						truthTuple.getValue("STATE")));
-			}
-			if (!checkSingleZIP(tuple)){
-				result.add(new RepairedCell(Integer.parseInt(tuple.getValue("RUID")), 
-						"ZIP", 
-						truthTuple.getValue("ZIP")));
-			}
-			//一致性检查
-			//checkCUIDConsistence(tuple, result);
 		}
 	
 		return result;
 	}
 	
-	/*
-	 * 一致性检查
-	 */
-	private static void checkCUIDConsistence(Tuple tuple, HashSet<RepairedCell> result) {
-		Tuple truthTuple = truthTuples.get(tuple.getValue("CUID"));
-		for (String field : FIELDS) {
-			String value = tuple.getValue(field);
-			String truthValue = truthTuple.getValue(field);
-			if (value != truthValue){
-				result.add(new RepairedCell(
-						Integer.parseInt(tuple.getValue("RUID")), 
-						field, 
-						truthValue));
-			}
-		}
-	}
-
 	/*
 	 * 初始化辅助数据
 	 * groupedTuples
@@ -132,25 +70,34 @@ public class DataProfolling {
 	 */
 	private static void initAux(LinkedList<Tuple> tuples) {
 		//根据FIELDS生成COLUMNNAMES
-		StringBuffer sb = new StringBuffer();
-		for (String field : FIELDS) {
-			sb.append(field).append(":");
-		}
-		sb.deleteCharAt(sb.length() - 1);
-		COLUMNNAMES = new ColumnNames(sb.toString());
+		generateColumnNames();
+		
 		//初始化groupedTuples
-		for (Tuple tuple : tuples){
-			String cuid = tuple.getValue("CUID");
-			if (groupedTuples.containsKey(cuid)) {
-				groupedTuples.get(cuid).add(tuple);
-			} else {
-				LinkedList<Tuple> list = new LinkedList<Tuple>();
-				list.add(tuple);
-				groupedTuples.put(cuid, list);
-			}
-		}
+		groupTuplesByCUID(tuples);
 		
 		//根据most vote原则，计算truthTuples
+		generateTruthTuples();
+		
+		checkAndRepairTruthTuples();
+		
+		
+	}
+
+	private static void checkAndRepairTruthTuples() {
+		for (Tuple tuple : truthTuples.values()){
+			StringBuffer sb = new StringBuffer();
+			for (String field : FIELDS){
+				sb.append(checkValue(field, tuple)).append(":");
+			}
+			sb.deleteCharAt(sb.length() - 1);
+			truthTuples.put(tuple.getValue("CUID"), 
+					new Tuple(COLUMNNAMES, sb.toString()));
+			truthTuples.remove(tuple);
+		}
+		
+	}
+
+	private static void generateTruthTuples() {
 		for (Entry entry : groupedTuples.entrySet()){
 			String cuid = (String)entry.getKey();
 			LinkedList<Tuple> list = (LinkedList<Tuple>)entry.getValue();
@@ -183,14 +130,84 @@ public class DataProfolling {
 		
 	}
 
+	private static void groupTuplesByCUID(LinkedList<Tuple> tuples) {
+		for (Tuple tuple : tuples){
+			String cuid = tuple.getValue("CUID");
+			if (groupedTuples.containsKey(cuid)) {
+				groupedTuples.get(cuid).add(tuple);
+			} else {
+				LinkedList<Tuple> list = new LinkedList<Tuple>();
+				list.add(tuple);
+				groupedTuples.put(cuid, list);
+			}
+		}
+	}
+
+	private static void generateColumnNames() {
+		StringBuffer sb = new StringBuffer();
+		for (String field : FIELDS) {
+			sb.append(field).append(":");
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		COLUMNNAMES = new ColumnNames(sb.toString());	
+	}
+
+	/*
+	 * 修复truthValue
+	 */
+	private static String checkValue(String field, Tuple tuple) {
+		String result = tuple.getValue(field);
+		switch (field){
+			case "CUID":
+				break;
+			case "SSN":
+				result = checkSingleSSN(tuple);
+				break;
+			case "FNAME":
+				result = checkSingleFNAME(tuple);
+				break;
+			case "MINIT":
+				result = checkSingleMINIT(tuple);
+				break;
+			case "LNAME":
+				result = checkSingleLNAME(tuple);
+				break;
+			case "STNUM":
+				result = checkSingleSTNUM(tuple);
+				break;
+			case "STADD":
+				result = checkSingleSTADD(tuple);
+				break;
+			case "APMT":
+				result = checkSingleAPMT(tuple);
+				break;
+			case "CITY":
+				result = checkSingleCITY(tuple);
+				break;
+			case "STATE":
+				result = checkSingleSTATE(tuple);
+				break;
+			case "ZIP":
+				result = checkSingleZIP(tuple);
+				break;	
+		}
+		return result;
+	}
+
 	/*
 	 * 邮政编码,五位纯数字
 	 */
-	private static boolean checkSingleZIP(Tuple tuple) {
+	private static String checkSingleZIP(Tuple tuple) {
+		String value = tuple.getValue("ZIP");
 		String regex = "^\\d{5}$";
 		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(tuple.getValue("ZIP"));
-		return m.matches();
+		Matcher m = p.matcher(value);
+		if (m.matches()){
+			return value;
+		} else {
+			//TODO
+			return "NeedPepair";
+		}
 
 	}
 
@@ -198,33 +215,55 @@ public class DataProfolling {
 	 * 为美国的50个州的简称
 	 * 这里的Metadata是错的！不光有50个州，还有DC(特区)，GU(关岛)等附属
 	 */
-	private static boolean checkSingleSTATE(Tuple tuple) {
-		return STATES.contains(tuple.getValue("STATE"));
+	private static String checkSingleSTATE(Tuple tuple) {
+		String value = tuple.getValue("STATE");
+		if (STATES.contains(value)) {
+			return value;
+		} else {
+			return value.toUpperCase();
+		}
 	}
+	
 
 	/*
 	 * 可能包含字母+五种标点符号'-/. (空格也算一种)
 	 */
-	private static boolean checkSingleCITY(Tuple tuple) {
+	private static String checkSingleCITY(Tuple tuple) {
+		String value = tuple.getValue("CITY");
 		String regex = "[a-zA-Z'-/\\. ]*";
 		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(tuple.getValue("CITY"));
-		return m.matches();
+		Matcher m = p.matcher(value);
+		if (m.matches()) {
+			return value;
+		} else {
+			//TODO
+			return "NeedPepair";		
+		}
 	}
 
 	/*
 	 * 公寓房号,3位,第1位和第3位为数字,第2位为小写字母.
 	 * STADD属性为“PO Box xxxx”时该属性为空
 	 */
-	private static boolean checkSingleAPMT(Tuple tuple) {
+	private static String checkSingleAPMT(Tuple tuple) {
 		String apmt = tuple.getValue("APMT");
 		if ( checkSTADDforSTNUMandAPMT(tuple) ) {
-			return apmt.isEmpty();
+			if (apmt.isEmpty()) {
+				return apmt;
+			} else {
+				//TODO
+				return "NeedPepair";
+			}
 		} else {
 			String regex = "^\\d[a-z]\\d$";
 			Pattern p = Pattern.compile(regex);
 			Matcher m = p.matcher(apmt);
-			return m.matches();
+			if (m.matches()){
+				return apmt;
+			} else {
+				//TODO
+				return "NeedPepair";
+			}
 		}	
 	}
 
@@ -232,22 +271,28 @@ public class DataProfolling {
 	 * 能包含字母,空格,逗号及句号,或者为“PO Box xxxx”,其中"xxxx"为1-4位纯数字.
 	 * 若为“PO Box xxxx”,则STNUM和APMT属性皆为空
 	 */
-	private static boolean checkSingleSTADD(Tuple tuple) {
+	private static String checkSingleSTADD(Tuple tuple) {
 		String stadd = tuple.getValue("STADD");
 		String regex = "[a-zA-Z, \\.]+";
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(stadd);
 		if ( m.matches() ){
-			return true;
+			return stadd;
 		}else{
 			regex = "PO Box \\d{1,4}";
 			p = Pattern.compile(regex);
 			m = p.matcher(stadd);
 			if (m.matches()){
-				return tuple.getValue("STNUM").isEmpty() && 
-						tuple.getValue("APMT").isEmpty();
+				if (tuple.getValue("STNUM").isEmpty() && 
+						tuple.getValue("APMT").isEmpty()) {
+					return stadd;
+				} else {
+					//TODO
+					return "NeedPepair";
+				}
 			} else {
-				return false;
+				//TODO
+				return "NeedPepair";
 			}
 		}
 	}
@@ -255,15 +300,25 @@ public class DataProfolling {
 	/*
 	 * 1-4位纯数字, STADD属性为“PO Box xxxx”时该属性为空
 	 */
-	private static boolean checkSingleSTNUM(Tuple tuple) {
+	private static String checkSingleSTNUM(Tuple tuple) {
 		String stunum = tuple.getValue("STNUM");
 		if ( checkSTADDforSTNUMandAPMT(tuple) ) {
-			return stunum.isEmpty();
+			if (stunum.isEmpty()) {
+				return stunum;
+			} else {
+				//TODO
+				return "NeedPepair";
+			}
 		} else {
 			String regex = "\\d{1,4}";
 			Pattern p = Pattern.compile(regex);
 			Matcher m = p.matcher(stunum);
-			return m.matches();
+			if (m.matches()) {
+				return stunum;
+			} else {
+				//TODO
+				return "NeedPepair";
+			}
 		}	
 	}
 
@@ -280,30 +335,47 @@ public class DataProfolling {
 	/*
 	 * 可能包含字母,逗号及句号,首字母大写
 	 */
-	private static boolean checkSingleLNAME(Tuple tuple) {
-		return checkNAME(tuple.getValue("LNAME"));
+	private static String checkSingleLNAME(Tuple tuple) {
+		String lname = tuple.getValue("LNAME");
+		if (checkNAME(lname)){
+			return lname;
+		} else {
+			//TODO
+			return "NeedPepair";
+		}
 	}
 
 	/*
 	 * 可为空,或为1位大写字母
 	 */
-	private static boolean checkSingleMINIT(Tuple tuple) {
+	private static String checkSingleMINIT(Tuple tuple) {
 		String minit = tuple.getValue("MINIT");
 		if (minit.isEmpty()){
-			return true;
+			return minit;
 		} else {
 			String regex = "[A-Z]{1}";
 			Pattern p = Pattern.compile(regex);
 			Matcher m = p.matcher(minit);
-			return m.matches();
+			if (m.matches()) {
+				return minit;
+			} else {
+				//TODO
+				return "NeedPepair";
+			}
 		}
 	}
 
 	/*
 	 * 可能包含字母,逗号及句号,首字母大写
 	 */
-	private static boolean checkSingleFNAME(Tuple tuple) {
-		return checkNAME(tuple.getValue("FNAME"));
+	private static String checkSingleFNAME(Tuple tuple) {
+		String fname = tuple.getValue("FNAME");
+		if (checkNAME(fname)){
+			return fname;
+		} else {
+			//TODO
+			return "NeedPepair";
+		}
 	}
 	
 	private static boolean checkNAME(String name){
@@ -316,11 +388,17 @@ public class DataProfolling {
 	/*
 	 * SSN 9 位纯数字
 	 * */
-	private static boolean checkSingleSSN(Tuple tuple) {
+	private static String checkSingleSSN(Tuple tuple) {
+		String ssn = tuple.getValue("SSN");
 		String regex = "\\d{9}";
 		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(tuple.getValue("SSN"));
-		return m.matches();
+		Matcher m = p.matcher(ssn);
+		if (m.matches()){
+			return ssn;
+		} else {
+			//TODO
+			return "NeedPepair";
+		}
 	}
 
 }
