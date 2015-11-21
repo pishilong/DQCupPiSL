a338183c1882f2c6f16eaf7334c07862534ecd81
pishilong <pishilong@gmail.com>
2015-11-22 05:08:34 +0800

    代码完成，但是似乎找到的FD是有问题的

--- src/dqcup/repair/impl/FDDiscover.java
+++ src/dqcup/repair/impl/FDDiscover.java
@@ -2,10 +2,13 @@ package dqcup.repair.impl;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import dqcup.repair.Tuple;
@@ -20,7 +23,7 @@ public class FDDiscover {
 	//Right Hand Sides plus
 	public static HashMap<Set<String>, Set<String>> rhs;
 	//顶点key
-	public static final Set<String> topRhs = new HashSet<String>(Arrays.asList("top"));
+	public static final Set<String> emptyRhs = new HashSet<String>(Arrays.asList("empty"));
 	
 	//Partition -> {A}, {{1, {1,2,3}}, {2, {4, 5}}}对值进行区分，并且保存CUID
 	public static HashMap<Set<String>, HashMap<Integer, Set<Integer>>> partition;
@@ -35,6 +38,7 @@ public class FDDiscover {
 		List<Set<String>> currentLevel = levels.getLast();
 		
 		while(!currentLevel.isEmpty()){
+			System.out.println("currentLevel:" + currentLevel.toString());
 			computeDependencies(currentLevel);
 			prune(currentLevel);
 			levels.addLast(generateNextLevel(currentLevel));
@@ -50,30 +54,28 @@ public class FDDiscover {
 		
 		for(List<Set<String>> prefixBlock : prefixBlocks){
 			//X=Y并Z
-			Set<String> block_0 = prefixBlock.get(0);
-			Set<String> block_1 = prefixBlock.get(1);
-			Set<String> wholeBlock = new HashSet<String>(block_0);
-			wholeBlock.addAll(block_1);
+			Set<String> Y = prefixBlock.get(0);
+			Set<String> Z = prefixBlock.get(1);
+			Set<String> X = new HashSet<String>(Y);
+			X.addAll(Z);
 			//计算partition[X] = product(partition[Y],partition[Z])
-			partition.put(wholeBlock, 
+			partition.put(X, 
 					strippedProduct(
-							partition.get(block_0),
-							partition.get(block_1)
+							partition.get(Y),
+							partition.get(Z)
 					));
 			
 			boolean flag = true;
-			for(String element : wholeBlock){
-				Set<String> dummy = new HashSet<String>(wholeBlock);
+			for(String element : X){
+				Set<String> dummy = new HashSet<String>(X);
 				dummy.remove(element);
 				if(!currentLevel.contains(dummy)){
 					flag = false;
 				}	
 			}
-			
-			if(flag){
-				nextLevel.add(wholeBlock);
+			if(flag && !nextLevel.contains(X)){
+				nextLevel.add(X);
 			}
-		
 		}
 		
 		return nextLevel;
@@ -90,14 +92,17 @@ public class FDDiscover {
 		HashMap<Integer, Set<Integer>> S = new HashMap<Integer, Set<Integer>>();
 		int index = 1;
 		
-		for(int i = 1; i < partition_1.size(); i++){
-			for(int cuid : partition_1.get(i)){
+		LinkedList<Set<Integer>> p1Values = new LinkedList<Set<Integer>>(partition_1.values());
+		LinkedList<Set<Integer>> p2Values = new LinkedList<Set<Integer>>(partition_2.values());
+		
+		for(int i = 1; i < p1Values.size(); i++){		
+			for(int cuid : p1Values.get(i)){
 				T.put(cuid, i);
 			}
 		}
 		
-		for(int i = 1; i < partition_2.size(); i++){
-			for(int cuid : partition_2.get(i)){
+		for(int i = 1; i < p2Values.size(); i++){
+			for(int cuid : p2Values.get(i)){
 				if (T.containsKey(cuid)){
 					int klass = T.get(cuid);
 					if (S.containsKey(klass)){
@@ -108,7 +113,7 @@ public class FDDiscover {
 				}
 			}
 			
-			for(int cuid : partition_2.get(i)){
+			for(int cuid : p2Values.get(i)){
 				Set<Integer> block = S.get(T.get(cuid));
 				if (block != null && block.size() > 1) {
 					result.put(index, block);
@@ -128,9 +133,9 @@ public class FDDiscover {
 	 */
 	private static List<List<Set<String>>> findPrefixBlocks(List<Set<String>> currentLevel) {
 		List<List<Set<String>>> result = new LinkedList<List<Set<String>>>();
-		for(int i = 0; i < currentLevel.size(); i ++){
+		for(int i = 0; i < currentLevel.size() - 1; i ++){
 			Set<String> ele1 = currentLevel.get(i);
-			for(int j = 0; j < currentLevel.size(); j ++){
+			for(int j = i + 1; j < currentLevel.size(); j ++){
 				Set<String> tempSet = new HashSet<String>(ele1);
 				Set<String> ele2 = currentLevel.get(j);
 				
@@ -151,7 +156,35 @@ public class FDDiscover {
 
 
 	private static void prune(List<Set<String>> currentLevel) {
-		//TODO superKey Logic
+		Iterator<Set<String>> it = currentLevel.iterator();
+		while(it.hasNext()){
+			Set<String> X = it.next();
+			if(rhs.get(X).isEmpty()){
+				it.remove();
+			} else {
+				// X is a superKey
+				if (partition.get(X).isEmpty()) {
+					Set<String> _set = new HashSet<String>(rhs.get(X));
+					_set.removeAll(X);
+					for (String A : _set) {
+						Set<String> wholeSet = new HashSet<String>(X);
+						Set<String> interSet = new HashSet<String>();
+						wholeSet.add(A);
+						for (String B : X) {
+							wholeSet.remove(B);
+							interSet.retainAll(rhs.get(wholeSet));
+						}
+						if (interSet.contains(A)) {
+							FD.put(A, X);
+							System.out.println("Function Dependency" + X.toString() + "->" + A);
+						}
+					}
+					it.remove();
+				}
+			}
+		}
+		//superKey Logic, X 的分区中所有等价类的size都是1，对于stripped partition来说，就是空
+		
 	}
 
 
@@ -167,7 +200,7 @@ public class FDDiscover {
 			Set<String> candidates = new HashSet<String>(elements);
 			candidates.retainAll(rhs.get(elements));
 			//R-X
-			Set<String> lastSet = new HashSet<String>(rhs.get(topRhs));
+			Set<String> lastSet = new HashSet<String>(rhs.get(emptyRhs));
 			lastSet.removeAll(elements);
 			
 			for(String candidate : candidates){
@@ -185,23 +218,52 @@ public class FDDiscover {
 	}
 
 
-	private static boolean checkValid(Set<String> fromSet, String candidate) {
+	//lemma 3.5 X->A holds if and only if e(X) = e(X union {A})
+	private static boolean checkValid(Set<String> X, String A) {
+		//X并{A}
+		Set<String> wholeSet = new HashSet<String>(X);	
+		wholeSet.add(A);
 		int e = 0;
-		Set<String> wholeSet = new HashSet<String>(fromSet);
-		HashMap<Integer, Integer> T = new HashMap<Integer, Integer>();
-		wholeSet.add(candidate);
-		for(Set<Integer> c : partition.get(wholeSet).values()){
-			T.put(c.iterator().next(), c.size());
-		}
-		for(Set<Integer> c : partition.get(fromSet).values()){
+		
+        HashMap<Integer, Integer> T = new HashMap<Integer, Integer>();
+        for(Set<Integer> c : partition.get(wholeSet).values()){
+        	T.put(c.iterator().next(), c.size());
+        }
+        for(Set<Integer> c : partition.get(X).values()){
 			int m = 1;
-			for (int cuid : c){
-				if (T.containsKey(cuid) && T.get(cuid) > m) m = T.get(cuid);
+			for (int cuid : c) {
+				if (T.containsKey(cuid) && T.get(cuid) > m)
+					m = T.get(cuid);
 			}
 			e += c.size() - m;
 		}
-		
-		return (double) e/(double) tuplesAmount > 0.9;
+        float result = (float)e/(float)tuplesAmount;
+        if(result < 0.001){
+        	System.out.println(X.toString()+"->"+A +": "+ result);
+        }
+        return result < 0.001;
+        
+		/*	
+		return caculateE(X) == caculateE(wholeSet);
+		*/
+	}
+
+
+	//在stripped partition的前提下，e(X) = (partion(X)中所有等价类的size总和 - partion(X)的等价类个数)/总的记录数
+	//stripped partition是在分区时，移除size 为 1的等价类
+	private static Object caculateE(Set<String> X) {
+		HashMap<Integer, Set<Integer>> strippedPartition = partition.get(X);
+		int sumSize = 0;
+		for(Set<Integer> eqClass : strippedPartition.values()){
+			sumSize += eqClass.size();
+		}
+		return (float)(sumSize - strippedPartition.size()) / (float)tuplesAmount;	
+	}
+
+
+	//判断是否子集
+	private static boolean isSubset(Set<Integer> src, Set<Integer> tar) {
+		return src.containsAll(tar);
 	}
 
 
@@ -213,7 +275,7 @@ public class FDDiscover {
 				Set<String> _elements = new HashSet<String>(elements);
 				_elements.remove(element);
 				//对每个rhsElement进行并集操作
-				if(_elements.isEmpty()) _elements = topRhs;
+				if(_elements.isEmpty()) _elements = emptyRhs;
 				Set<String> rhsElement = rhs.get(_elements);
 				if(result.isEmpty()){
 					result.addAll(rhsElement);
@@ -239,10 +301,11 @@ public class FDDiscover {
 		
 		rhs = new HashMap<Set<String>, Set<String>>();
 		//顶点
-		rhs.put(topRhs, 
+		rhs.put(emptyRhs, 
 				new HashSet<String>(Arrays.asList(DataProfolling.FIELDS)));
 		
 		//分区
+		//Stripped partition需要移除size 为1的等价类
 		partition = new HashMap<Set<String>, HashMap<Integer, Set<Integer>>>();
 		
 		Collection<Tuple> tuples = truthTuples.values();
@@ -271,11 +334,19 @@ public class FDDiscover {
 					result.put(klass, cuidSet);
 				}
 			}
+			
+			Iterator<Entry<Integer, Set<Integer>>>  it = result.entrySet().iterator();
+			while(it.hasNext()){
+				Entry<Integer, Set<Integer>> entry = it.next();
+				if(entry.getValue().size() == 1){
+					it.remove();
+				}
+			}
 			partition.put(element, result);
 		}
 		
 		tuplesAmount = truthTuples.size();
 		
 	}
-
+	
 }
